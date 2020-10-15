40a3216c6f35c96b4086489df3fdea1b24495aa3
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-10-15 15:02:42 -0700

    completed declare string variables challenge

---
+++ JS/Basic-Javascript/Declare-String-Variables.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * Basic JavaScript: Declare String Variables
+Previously we have used the code
+
+var myName = "your name";
+
+"your name" is called a string literal. It is a string because it is a series of zero or more characters enclosed in single or double quotes.
+
+Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+myFirstName should be a string with at least one character in it.
+
+myLastName should be a string with at least one character in it.
+
+ */
+
+
+
+
+
+
+var myFirstName = "Will";
+var myLastName = "Johnson";
