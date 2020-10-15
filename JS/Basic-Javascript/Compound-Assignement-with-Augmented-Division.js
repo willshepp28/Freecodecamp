30106fb8094fc8e530964bb6d8b9a21e06ee7d92
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-10-15 15:00:45 -0700

    completed compound assignment with augmented division challenge

---
+++ JS/Basic-Javascript/Compound-Assignement-with-Augmented-Division.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ * 
+ * Basic JavaScript: Compound Assignment With Augmented Division
+The /= operator divides a variable by another number.
+
+myVar = myVar / 5;
+
+Will divide myVar by 5. This can be rewritten as:
+
+myVar /= 5;
+
+Convert the assignments for a, b, and c to use the /= operator.
+
+
+
+a should equal 4.
+
+b should equal 27.
+
+c should equal 3.
+
+You should use the /= operator for each variable.
+
+You should not modify the code above the specified comment.
+
+ */
+
+
+
+
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
