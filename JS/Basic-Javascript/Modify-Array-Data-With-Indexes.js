7e90a6148c85c721c6995380dd70c3c2978e2d23
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-10-15 16:01:30 -0700

    completed modify array data with indexes challenge

---
+++ JS/Basic-Javascript/Modify-Array-Data-With-Indexes.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * 
+ * Basic JavaScript: Modify Array Data With Indexes
+Unlike strings, the entries of arrays are mutable and can be changed freely.
+
+Example
+
+var ourArray = [50,40,30];
+ourArray[0] = 15; // equals [15,40,30]
+Note
+There shouldn't be any spaces between the array name and the square brackets, like array [0]. Although JavaScript is able to process this correctly, this may confuse other programmers reading your code.
+
+Modify the data stored at index 0 of myArray to a value of 45.
+
+
+
+myArray should now be [45,64,99].
+
+You should be using correct index to modify the value in myArray.
+
+ */
+
+
+
+ // Setup
+var myArray = [18,64,99];
+
+// Only change code below this line
+
+myArray[0] = 45;
