f4ec7bd0d9b5d7ed2ad26bb3273b373eb5e618f0
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-10-15 13:49:07 -0700

    compleded introduction to javascript

---
+++ JS/Basic-Javascript/Introduction-to-Javascript.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * 
+ * Comments are lines of code that JavaScript will intentionally ignore. Comments are a great way to leave notes to yourself and to other people who will later need to figure out what that code does.
+
+There are two ways to write comments in JavaScript:
+
+Using // will tell JavaScript to ignore the remainder of the text on the current line:
+
+
+
+
+    CHALLENGE:
+    -You should create a // style comment that contains at least five letters.
+
+    - You should create a style comment that contains at least five letters.
+ */
+
+
+
+
+ // This is a comment
+
+ /**
+  * 
+  * 
+  * This is also a comment.
+  */
+
