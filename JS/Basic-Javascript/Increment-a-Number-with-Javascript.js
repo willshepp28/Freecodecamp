0adc7e8d6048163d305d247496e0f64aabe5b321
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-10-15 14:16:06 -0700

    completed increment-one-number-with-javascript challenge

---
+++ JS/Basic-Javascript/Increment-a-Number-with-Javascript.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ * Basic JavaScript: Increment a Number with JavaScript
+You can easily increment or add one to a variable with the ++ operator.
+
+i++;
+
+is the equivalent of
+
+i = i + 1;
+
+Note
+The entire line becomes i++;, eliminating the need for the equal sign.
+
+Change the code to use the ++ operator on myVar.
+
+
+
+
+    CHALLENGE:
+
+    -  myVar should equal 88.
+
+    - You should not use the assignment operator.
+
+    - You should use the ++ operator.
+
+    - You should not change code above the specified comment.
+ */
+
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
+
