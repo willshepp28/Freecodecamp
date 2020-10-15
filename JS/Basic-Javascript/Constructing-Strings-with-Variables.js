9ec0120008718bbf5540221ba90a31b1eb34e154
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-10-15 15:29:22 -0700

    completed constructing strings with variables challenge

---
+++ JS/Basic-Javascript/Constructing-Strings-with-Variables.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * Basic JavaScript: Constructing Strings with Variables
+Sometimes you will need to build a string, Mad Libs style. By using the concatenation operator (+), you can insert one or more variables into a string you're building.
+
+Example:
+
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+// ourStr is now "Hello, our name is freeCodeCamp, how are you?"
+Set myName to a string equal to your name and build myStr with myName between the strings "My name is " and " and I am well!"
+
+myName should be set to a string at least 3 characters long.
+
+You should use two + operators to build myStr with myName inside it.
+
+ */
+
+
+
+
+// Only change code below this line
+var myName = "Will";
+var myStr = "My name is " + myName + " and I am well!";
