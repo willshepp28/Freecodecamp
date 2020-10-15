d40dd567bc87da5ee9cff16b1d29efdb4d014479
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-10-15 14:57:07 -0700

    completed compund assignemnt with augmented multiplication challenge

---
+++ JS/Basic-Javascript/Compound-Assigment-with-Augmented-Subtraction.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ * 
+ * Basic JavaScript: Compound Assignment With Augmented Subtraction
+Like the += operator, -= subtracts a number from a variable.
+
+myVar = myVar - 5;
+
+will subtract 5 from myVar. This can be rewritten as:
+
+myVar -= 5;
+
+Convert the assignments for a, b, and c to use the -= operator.
+
+
+a should equal 5.
+
+b should equal -6.
+
+c should equal 2.
+
+You should use the -= operator for each variable.
+
+You should not modify the code above the specified comment.
+ */
+
+
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
