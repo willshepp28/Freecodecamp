63f6bc102a46f73ed451f035f3becee1c0b97d0a
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-10-15 14:54:43 -0700

    completed compound assignment with augmented addtion challenge

---
+++ JS/Basic-Javascript/Compound-Assignment-with-Augmentd-Addition.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ * 
+ * Basic JavaScript: Compound Assignment With Augmented Addition
+In programming, it is common to use assignments to modify the contents of a variable. Remember that everything to the right of the equals sign is evaluated first, so we can say:
+
+myVar = myVar + 5;
+
+to add 5 to myVar. Since this is such a common pattern, there are operators which do both a mathematical operation and assignment in one step.
+
+One such operator is the += operator.
+
+var myVar = 1;
+myVar += 5;
+console.log(myVar); // Returns 6
+Convert the assignments for a, b, and c to use the += operator.
+
+
+a should equal 15.
+
+b should equal 26.
+
+c should equal 19.
+
+You should use the += operator for each variable.
+
+You should not modify the code above the specified comment.
+ */
+
+
+
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
