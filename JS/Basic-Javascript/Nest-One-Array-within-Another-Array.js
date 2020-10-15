75b0fe4e3b4ea7b480565b8d81689f861d12a76b
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-10-15 15:55:47 -0700

    completed nest one array within anthoer array challenge

---
+++ JS/Basic-Javascript/Nest-One-Array-within-Another-Array.js
@@ -0,0 +1,17 @@
+/**
+ * Basic JavaScript: Nest one Array within Another Array
+You can also nest arrays within other arrays, like below:
+
+[["Bulls", 23], ["White Sox", 45]]
+This is also called a multi-dimensional array.
+
+Create a nested array called myArray.
+ * 
+
+ myArray should have at least one array nested within another array.
+ */
+
+
+
+ // Only change code below this line
+var myArray = [[]];
