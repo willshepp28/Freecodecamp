4cf74f25611aeca0af70f5ca332fe414537cf57e
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-10-15 14:58:54 -0700

    completed compound assignement with augmented muliplication challenge

---
+++ JS/Basic-Javascript/Compound-Assignement-with-Augmented-Multiplication.js
@@ -0,0 +1,41 @@
+/**
+ * 
+ * Basic JavaScript: Compound Assignment With Augmented Multiplication
+The *= operator multiplies a variable by a number.
+
+myVar = myVar * 5;
+
+will multiply myVar by 5. This can be rewritten as:
+
+myVar *= 5;
+
+Convert the assignments for a, b, and c to use the *= operator.
+
+
+a should equal 25.
+
+b should equal 36.
+
+c should equal 46.
+
+You should use the *= operator for each variable.
+
+You should not modify the code above the specified comment.
+
+ */
+
+
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a = a * 5;
+b = 3 * b;
+c = c * 10;
+
+
+a *= 5;
+b *= 3;
+c *= 10;
