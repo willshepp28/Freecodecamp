aa9a906d94f1ff62b95ab24608050e5c480acf11
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-10-15 15:32:03 -0700

    completed appending variables to strings challenge

---
+++ JS/Basic-Javascript/Appending-Variables-to-Strings.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * Basic JavaScript: Appending Variables to Strings
+Just as we can build a string over multiple lines out of string literals, we can also append variables to a string using the plus equals (+=) operator.
+
+Example:
+
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+// ourStr is now "freeCodeCamp is awesome!"
+Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+
+
+someAdjective should be set to a string at least 3 characters long.
+
+You should append someAdjective to myStr using the += operator.
+ */
+
+
+// Change code below this line
+
+var someAdjective = "fun";
+var myStr = "Learning to code is ";
+
+myStr += someAdjective;
