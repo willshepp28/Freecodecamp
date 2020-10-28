b0c4c829e532e477a4b0f2ca2f178a4aec70321a
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-10-28 11:36:52 -0700

    completed use the parseInt function with a radix

---
+++ JS/Basic-Javascript/Use-the-ParseInt-Function-with-a-Radix.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * Basic JavaScript: Use the parseInt Function with a Radix
+The parseInt() function parses a string and returns an integer. It takes a second argument for the radix, which specifies the base of the number in the string. The radix can be an integer between 2 and 36.
+
+The function call looks like:
+
+parseInt(string, radix);
+
+And here's an example:
+
+var a = parseInt("11", 2);
+
+The radix variable says that "11" is in the binary system, or base 2. This example converts the string "11" to an integer 3.
+
+Use parseInt() in the convertToInteger function so it converts a binary number to an integer and returns it.
+
+
+convertToInteger should use the parseInt() function
+
+convertToInteger("10011") should return a number
+
+convertToInteger("10011") should return 19
+
+convertToInteger("111001") should return 57
+
+convertToInteger("JamesBond") should return NaN
+
+
+ * 
+ */
+
+
+
+function convertToInteger(str) {
+    return parseInt(str, 2)
+}
+
+convertToInteger("10011");
